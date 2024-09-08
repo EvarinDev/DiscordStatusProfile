8c916771b8a8dc52e989f85da179ec8f3eaec3fb
FAYStarNext <phasordev@gmail.com>
2024-09-08 05:17:03 +0000

    Add @radix-ui/react-toast package for displaying toasts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,17 @@
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { toast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
+
+function copyText(text: string) {
+  return () => {
+    navigator.clipboard.writeText(text);
+    toast({
+      title: "Copied to clipboard",
+    });
+  };
+}
 
 export default function Home() {
   const [discordId, setDiscordId] = useState("");
@@ -18,33 +29,34 @@ export default function Home() {
     }
   };
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <div className="items-center text-center">
-        <div className="text-lg mt-2">
-          <h1 className="text-4xl font-bold">Discord Status Profile</h1>
-          <p className="text-gray-500">Join <a href="https://discord.gg/s3j5vphByS" className="text-blue-500">Discord</a> to use it</p>
-          <p className="text-gray-500 mb-6">Display your Discord Presence in your GitHub Profile</p>
-        </div>
-        <div className="flex w-full max-w-sm items-center space-x-2">
-          <Input
-            type="number"
-            placeholder="Discord ID"
-            value={discordId}
-            onChange={(e) => setDiscordId(e.target.value)}
-          />
-          <Button type="button" onClick={handleGenerate}>
-            Generator
-          </Button>
-        </div>
-        {generatedImage && (
-          <div className="border border-gray-500 p-4 mt-4">
-            <div
-              dangerouslySetInnerHTML={{ __html: generatedImage }}
-              className="mt-4"
-            />
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="items-center text-center">
+          <div className="text-lg mt-2">
+            <h1 className="text-4xl font-bold">Discord Status Profile</h1>
+            <p className="text-gray-500">Join <a href="https://discord.gg/s3j5vphByS" className="text-blue-500">Discord</a> to use it</p>
+            <p className="text-gray-500 mb-6">Display your Discord Presence in your GitHub Profile</p>
           </div>
-        )}
+          <div className="flex w-full max-w-sm items-center space-x-2">
+            <Input
+              type="number"
+              placeholder="Discord ID"
+              value={discordId}
+              onChange={(e) => setDiscordId(e.target.value)}
+            />
+            <Button type="button" onClick={handleGenerate}>
+              Generator
+            </Button>
+          </div>
+          {generatedImage && (
+            <div className="border border-gray-500 p-4 mt-4">
+              <div
+                dangerouslySetInnerHTML={{ __html: generatedImage }}
+                className="mt-4"
+              />
+              <Button type="button" onClick={copyText(`${window.location}users?id=${discordId}`)}>Copy</Button>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
-  );
+    );
 }
